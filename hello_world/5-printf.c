8f3fc900a0d7ccd5e63a227a540971e21aa24f73
prescilliach <https://github.com/prescilliach/holbertonschool-low_level_programming>
2025-10-22 23:14:12 +0200

    add printf function file

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a sentence using the printf function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
