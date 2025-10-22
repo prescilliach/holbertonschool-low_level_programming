596e9a8abdc1cd9b60b91f4045fce5c454aec034
prescilliach <https://github.com/prescilliach/holbertonschool-low_level_programming>
2025-10-22 23:49:51 +0200

    add size various file

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the size of various types on the computer it is compiled and run on
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+	printf("Size of a int: %d byte(s)\n", (int) sizeof(char));
+	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
+	return (0);
+}
+
