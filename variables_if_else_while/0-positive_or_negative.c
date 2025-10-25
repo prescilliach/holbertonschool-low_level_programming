7497700a0b79d6ea73238e1c42125bd250231b62
prescilliach <https://github.com/prescilliach/holbertonschool-low_level_programming>
2025-10-26 00:29:23 +0200

    add positive_or_negative.c in variables_if_else_while

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,22 +14,6 @@ int main(void)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-	
-	n = -25;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
-
-	n = 0;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
 
 	return 0;
 }
