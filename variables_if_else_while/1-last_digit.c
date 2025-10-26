988865a1870713205aa729573249fec51acc74fd
prescilliach <https://github.com/prescilliach/holbertonschool-low_level_programming>
2025-10-26 17:34:11 +0100

    add last_digit in variables_if_else_while

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
  * main - Prints the last digit of a randum generated number
  * and it is greater than 5, less than 6, or 0.
  *
- * Return: 0
+ * Return: Always 0.
 */
 int main(void)
 {
@@ -18,18 +18,18 @@ int main(void)
 	if ((n % 10) > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n",
-				n, n % 10);
+				n, last_digit);
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-				n, n % 10);
+				n, last_digit);
 	}
 	else 
 	{
 		printf("Last digit of %d is %d and is 0\n",
-				n , n % 10);
+				n , last_digit);
 	}
 
-	return 0;
+	return (0);
 }
