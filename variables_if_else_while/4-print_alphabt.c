fb751cd5cd7b7dbf0bb1ccc68a130e828c88a9c6
prescilliach <https://github.com/prescilliach/holbertonschool-low_level_programming>
2025-10-26 12:54:18 +0100

    add alphabet_except in variables_if_else_while

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase, for letter except q and e.
+ * main - Prints the alphabet in lowercase, for except q and e.
  *
  * Return: Always 0.
  */
@@ -10,7 +10,6 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
 	{
 		if (letter != 'e' && letter != 'q')
 		putchar(letter);
