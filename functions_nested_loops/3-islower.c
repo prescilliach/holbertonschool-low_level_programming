ee8f3c1d226ea5e618093c7600d7896777c9cc80
prescilliach <https://github.com/prescilliach/holbertonschool-low_level_programming>
2025-10-28 10:16:03 +0100

    Update islower and main.h; remove old file

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -4,7 +4,7 @@
  * _islower - Checks if a character is lowercase.
  * @c: The character to check.
  *
- * Return 1 if a character is lowercase, 0 otherwise.
+ * Return: 1 if a character is lowercase, 0 otherwise.
  */
 int _islower(int c)
 {
